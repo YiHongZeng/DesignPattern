3854bbc1a68414f76657408aa866411a304cbc44
yihong.zeng <1054995637@qq.com>
2020-01-15 11:30:06 +0800

    代码保存

--- Factory/index.php
+++ Factory/index.php
@@ -3,11 +3,15 @@ namespace Factory;
 
 include '../autoload.php';
 
-
-
 /**
  * 工厂方法模式
+ * 1、通过一个抽象的核心工厂类指定方法.
+ * 2、具体工厂类实现抽象工厂类方法.
+ * 3、创建一个支付接口
+ * 4、支付具体类实现接口
  */
+
+
 $pay_amount = 10;
 
 $factory = new WechatFactory();
